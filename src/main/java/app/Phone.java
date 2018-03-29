467c20d6883a668118afd6ce222cf0e0f99ac9bf
mikhailapanovich <opanovichmv@gmail.com>
2018-03-29 11:35:01 +0200

    feature #5: phone validation

---
+++ src/main/java/app/Phone.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Phone {
+	// Валидация телефонного номера.
+	public static boolean validatePhoneNumber(String phoneNumber)
+	{
+    	Matcher matcher = Pattern.compile("[\\d+#-]{2,50}").matcher(phoneNumber);
+    	return matcher.matches();  
+	}
+}
